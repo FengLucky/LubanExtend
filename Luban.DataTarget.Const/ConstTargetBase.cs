42abab1e01326183d56fdceec13fd3a665f95127
luckyfeng <1101459726@qq.com>
2024-09-21 04:29:06 +0800

    导出C#常量

---
+++ Luban.DataTarget.Const/ConstTargetBase.cs
@@ -0,0 +1,26 @@
+﻿using Luban.Defs;
+
+namespace Luban.DataTarget.Const;
+
+public abstract class ConstTargetBase : DataTargetBase
+{
+    public override OutputFile ExportTable(DefTable table, List<Record> records)
+    {
+        //  var constIndex = table.fi.IndexOf(record=> record)
+        var constIndex = table.ValueTType.DefBean.Fields.FindIndex(field => field.Groups.FindIndex(group => group == "const") > -1 && field.CType.TypeName == "string");
+        if (constIndex == -1)
+        {
+            
+            return null;
+        }
+
+        var content = GenCode(table, records, constIndex);
+        if (content == null)
+        {
+            return null;
+        }
+        return new OutputFile { File = $"{table.Name}Const.{OutputFileExt}", Content = content };
+    }
+
+    protected abstract string? GenCode(DefTable table, List<Record> records, int constIndex);
+}
